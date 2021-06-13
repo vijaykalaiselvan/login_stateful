405698f77cc191a5e1c7596821a146ac212f5c2e
Vijay <vijay@starnik.com>
2021-06-13 17:33:40 +0530

    Completed the login screen with StatefullWidget

--- lib/src/screens/login_screen.dart
+++ lib/src/screens/login_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import '../mixins/validation_mixin.dart';
 
 class LoginScreen extends StatefulWidget {
   @override
@@ -7,16 +8,22 @@ class LoginScreen extends StatefulWidget {
   }
 }
 
-class LoginScreenState extends State<LoginScreen> {
+class LoginScreenState extends State<LoginScreen> with ValidationMixin {
+  final formKey = GlobalKey<FormState>();
+
+  String? email = '';
+  String? password = '';
   @override
   Widget build(BuildContext context) {
     return Container(
       margin: const EdgeInsets.all(20.0),
       child: Form(
+        key: formKey,
         child: Column(
           children: [
             emailField(),
             passwordField(),
+            Container(margin: const EdgeInsets.only(top: 25.0)),
             submitButton(),
           ],
         ),
@@ -31,6 +38,10 @@ class LoginScreenState extends State<LoginScreen> {
         labelText: 'Email Address',
         hintText: 'you@example.com',
       ),
+      validator: validateEmail,
+      onSaved: (String? value) {
+        email = value;
+      },
     );
   }
 
@@ -41,15 +52,24 @@ class LoginScreenState extends State<LoginScreen> {
         labelText: 'Password',
         hintText: 'Password',
       ),
+      validator: validatePassword,
+      onSaved: (String? value) {
+        password = value;
+      },
     );
   }
 
   Widget submitButton() {
     return ElevatedButton(
       child: const Text('Sumbit!'),
-      onPressed: () {},
+      onPressed: () {
+        if (formKey.currentState!.validate()) {
+          formKey.currentState!.save();
+          print('Time to $email and $password to my API');
+        }
+      },
       style: ButtonStyle(
-        backgroundColor: MaterialStateProperty.all<Color>(Colors.blue),
+        backgroundColor: MaterialStateProperty.all<Color>(Colors.green),
       ),
     );
   }
